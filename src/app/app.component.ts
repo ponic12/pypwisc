f1a546f6d8e267e25572dc37b7c8278f5a5f3545
Pablo <pmassad@yahoo.com>
2017-08-30 02:19:24 +0000

    fix interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component , OnInit} from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+  trigger('cardFlip', [
+      state('inactive', style({
+        backgroundColor: '#eee',
+        transform: 'scale(1)'
+      })),
+      state('active',   style({
+        backgroundColor: '#cfd8dc',
+        transform: 'scale(1.1)'
+      })),
+      transition('inactive => active', animate('500ms ease-in')),
+      transition('active => inactive', animate('500ms ease-out'))
+    ])
+  ]
 })
 export class AppComponent implements OnInit{
   constructor() { }
 
+  disableButton: boolean = true;
+  
   colors: Array<string> = ["red", "green", "blue", "yellow"];
   shapes: Array<string> =["cross", "star", "circle", "triangle"];
   numbers: Array<number> =[1,2,3,4];
@@ -35,12 +58,15 @@ export class AppComponent implements OnInit{
   };
   
   startTest() : void {
+    this.disableButton = false;
     this.cardsCounter = 0;
     this.sel = this.cards[this.cardsCounter]; //{nro:4, shape:'triangle', color:'green'};
     this.cardsCounter++;
   };
   
   evalSel(c) : void {
+      this.disableButton = true;
+      setInterval(()=>this.disableButton=false, 1000);
       let serie = this.series[this.seriesIndex];
       if (((serie == 'color')&&(this.sel.color == c.color)) ||
           ((serie == 'shape')&&(this.sel.shape == c.shape)) ||
@@ -49,14 +75,28 @@ export class AppComponent implements OnInit{
       {
         this.result.correct = this.result.correct + 1;
         this.result.partialOK++;
-        if (this.result.partialOK == 10) 
-              this.seriesIndex++;
+        if (this.result.partialOK == 10) {
+          this.result.perseverations--; // previene la perseveracion en el cambio de serie
+          this.result.partialOK = 0;
+          this.seriesIndex++;
+          if (this.seriesIndex > 6)
+            this.cardsCounter = 127;
+        }
       }
       else {
         this.result.partialOK = 0;
         this.result.incorrect = this.result.incorrect + 1;
-      }
+        
+        if (((serie == 'color')&&(this.sel.nro == c.nro)&&(this.seriesIndex==3)) ||
+            ((serie == 'shape')&&(this.sel.color == c.color)) ||
+            ((serie == 'number')&&(this.sel.shape == c.shape))
+        )
+        {
+          this.result.perseverations++;
+        }
+      };
 
+      this.sel = null;
       this.cardsCounter++;
       this.sel = this.cards[this.cardsCounter]; 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './testing/card/card.component';
@@ -10,7 +11,8 @@ import { CardComponent } from './testing/card/card.component';
     CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
